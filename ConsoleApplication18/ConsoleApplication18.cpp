494e580e44f7abdf2130c50c0ba7511af3004149
xavicp <xavier-aleix.cristofol@estudiantat.upc.edu>
2025-01-12 12:11:29 +0100

    Update ConsoleApplication18.cpp

--- ConsoleApplication18/ConsoleApplication18.cpp
+++ ConsoleApplication18/ConsoleApplication18.cpp
@@ -2,10 +2,47 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// Clase base abstracta Animal
+class Animal {
+public:
+    virtual void speak() const = 0; // Método abstracto
+    virtual ~Animal() {} // Destructor virtual
+};
+
+// Clase derivada Dog
+class Dog : public Animal {
+public:
+    void speak() const override {
+        cout << "Woof! Woof!" << endl;
+    }
+};
+
+// Clase derivada Cat
+class Cat : public Animal {
+public:
+    void speak() const override {
+        cout << "Meow! Meow!" << endl;
+    }
+};
+
+int main() {
+    Animal* dog = new Dog();
+    Animal* cat = new Cat();
+
+    cout << "El perro dice: ";
+    dog->speak();
+
+    cout << "El gato dice: ";
+    cat->speak();
+
+    // Liberar memoria
+    delete dog;
+    delete cat;
+
+    return 0;
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
